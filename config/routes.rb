7fd8853ce9105ad288c15142ac1b0c0c13b70183
Bella Rose Wilson <bwilson9893@gmail.com>
2025-07-10 19:00:14 +0000

    saving to pause, figure out the controller errors

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
-  root "dice#index"
+  root "dice#index"  # maps "/" to dice#index
+
+  # dynamic dice route: /dice/2/6, /dice/5/20, etc.
   get "/dice/:number/:sides", to: "dice#roll"
 end
