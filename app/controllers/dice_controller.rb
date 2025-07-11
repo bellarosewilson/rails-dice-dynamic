4f2afaca849feaf90af70bdc2454ff0bb7692c99
Bella Rose Wilson <bwilson9893@gmail.com>
2025-07-11 22:33:13 +0000

    controller dice fix

--- app/controllers/dice_controller.rb
+++ app/controllers/dice_controller.rb
@@ -1,19 +1,13 @@
-# app/controllers/pages_controller.rb
-class PagesController < ApplicationController
-  def home
-    render({ template: 'page_templates/index' })
+class DiceController < ApplicationController
+  def index
+    render :index
   end
 
   def roll
-    dice = params.fetch('num_of_dice')
-    sides = params.fetch('num_of_sides')
+    @number = params[:number].to_i
+    @sides = params[:sides].to_i
+    @rolls = Array.new(@number) { rand(1..@sides) }
 
-    @dice = dice.to_i
-    @sides = sides.to_i
-    @rolls = []
-
-    @dice.times { @rolls.push(roll = rand(1..@sides)) }
-
-    render({ template: 'page_templates/roll' })
+    render :roll
   end
 end
